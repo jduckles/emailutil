77c85ad772247f286c64193244d4321d67bd673c
Jonah Duckles <jonah@duckles.org>
2018-09-13 10:24:02 +1200

    refining flags

--- send_email.py
+++ send_email.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import util
 import sys
 import argparse
@@ -9,8 +10,10 @@ def main():
         email_body = template.render(item)
         try:
             print("Sent to " + item['email'])
-            print(email_body)
-            util.send_email(item['email'],subject, email_body)
+            if not args.quiet:
+                print(email_body)
+            if not args.dryrun:
+                util.send_email(item['email'],subject, email_body)
         except:
             print("missing email address")
 
@@ -20,6 +23,8 @@ if __name__ == '__main__':
     parser.add_argument("context", help="Path to a CSV file with the data context for the template. Each row in the CSV will result in one sent email. The template is filled with associated data from a given row. Must have a column named 'email' for To: address.")
     parser.add_argument("template",help="Path to a Jinja2 template file that can be filled by the context")
     parser.add_argument("subject", help="Quoted string with email's subject line, subject string is NOT filled as a template")
+    parser.add_argument("-q","--quiet", help="Supress printing of all filled templates.", action="store_true")
+    parser.add_argument("-d", "--dryrun", help="Don't actually send emails, just print them to screen.", action="store_true")
     args = parser.parse_args()
     context = util.csv2dict(args.context)
     template = util.readtemplate(args.template)
