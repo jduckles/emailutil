91d69b8b5252f039bb3eb5e0052fb7d8a1e0b2da
Jonah Duckles <jonah@duckles.org>
2018-09-13 10:13:48 +1200

    Adding argparse and help text

--- send_email.py
+++ send_email.py
@@ -1,5 +1,7 @@
 import util
 import sys
+import argparse
+
 
 
 def main():
@@ -14,7 +16,12 @@ def main():
 
 
 if __name__ == '__main__':
-    context = util.csv2dict(sys.argv[1])
-    template = util.readtemplate(sys.argv[2])
-    subject= sys.argv[3]
+    parser = argparse.ArgumentParser()
+    parser.add_argument("context", help="Path to a CSV file with the data context for the template. Each row in the CSV will result in one sent email. The template is filled with associated data from a given row. Must have a column named 'email' for To: address.")
+    parser.add_argument("template",help="Path to a Jinja2 template file that can be filled by the context")
+    parser.add_argument("subject", help="Quoted string with email's subject line, subject string is NOT filled as a template")
+    args = parser.parse_args()
+    context = util.csv2dict(args.context)
+    template = util.readtemplate(args.template)
+    subject = args.subject
     main()
